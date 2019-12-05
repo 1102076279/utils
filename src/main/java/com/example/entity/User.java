2929ef0f82593a6d67829a66276120d900ffafa0
1102076279 <45119408+1102076279@users.noreply.github.com>
2019-12-05 09:50:19 +0800

    first commit

---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,45 @@
+package com.example.entity;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private Short age;
+
+    private Date birthday;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Short getAge() {
+        return age;
+    }
+
+    public void setAge(Short age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
